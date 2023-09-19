64698b8a9ee563100d271bfe35bb714d243e6f42
Sunil Kumar Pal <sunil4sp@gmail.com>
2023-09-19 21:26:27 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from "react";
-/* import ReactDOM from "react-dom"; */
+import ReactDOM from "react-dom";
 import App from "./components/App";
-import { createRoot } from 'react-dom/client';
+/* import { createRoot } from 'react-dom/client'; */
 import DataApp from "./components/DataApp";
 
-const root = createRoot(document.getElementById('root'));
+/* const root = createRoot(document.getElementById('root'));
 
 root.render(
   <DataApp>
     <App />
   </DataApp>  
-);
+); */
 
 
-/* ReactDOM.render(
+ReactDOM.render(
     <DataApp>
       <App />
     </DataApp>, 
-document.getElementById("root")); */
+document.getElementById("root"));
